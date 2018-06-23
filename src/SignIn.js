8bb4ace9bb00344a83c51c4f4a026684fc2e5625
Elijah Basile <berlinboy9@yahoo.com>
2018-06-22 22:28:13 -0400

    create static template for conceptual design

---
+++ src/SignIn.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class SignIn extends Component {
+  render() {
+    return (
+        <div className="signIn">
+          <header className="heading">
+
+          </header>
+          <p className="welcome">Welcome to MedManage</p>
+          <p className="description">Where one can manage their prescriptions and doctor information,<br />as well as a calendar to manage their needs</p>
+        </div>
+      )
+  }
+}
